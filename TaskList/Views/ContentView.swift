64d92e0b59c7a999ef5369a5646c8951a20cd2bc
Rudi Farkas <rudi.farkas@gmail.com>
2020-09-01 18:50:20 +0200

    ContentView: present preliminary IMessagesView cloned from MailView: builds; first messages sent

--- TaskList/Views/ContentView.swift
+++ TaskList/Views/ContentView.swift
@@ -26,13 +26,15 @@
 /// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 /// THE SOFTWARE.
 
-import SwiftUI
 import MessageUI
+import SwiftUI
 
 // swiftlint:disable multiple_closures_with_trailing_closure
 struct ContentView: View {
   @ObservedObject var taskStore: TaskStore
-  @State var result: Result<MFMailComposeResult, Error>?
+  @State var mailComposeResult: Result<MFMailComposeResult, Error>?
+  //@State var messageComposeResult: Result<MessageComposeResult, Error>?
+  // can't use messageComposeResult because  2 controllers presented in HStack must have same init signature
   @State var modalIsPresented = false
   @State var mailViewIsPresented = false
   @State var iMessagesViewIsPresented = false
@@ -59,6 +61,7 @@ struct ContentView: View {
           .sheet(isPresented: $modalIsPresented) {
             NewTaskView(taskStore: self.taskStore)
           }
+
           // Export Via Email
           Button(action: { self.mailViewIsPresented = true }) {
             Image(systemName: "envelope")
@@ -70,23 +73,27 @@ struct ContentView: View {
               messageBody: "This is a test email string",
               attachmentInfo: (
                 fileURL: TaskStore.shared.tasksDocURL,
-                mimeType: "application/xml"),
-              result: self.$result)
+                mimeType: "application/xml"
+              ),
+              result: self.$mailComposeResult
+            )
           }
-          
+
           // Export Via iMessages
           Button(action: { self.iMessagesViewIsPresented = true }) {
             Image(systemName: "square.and.pencil")
           }
           .frame(width: 44, height: 44, alignment: .center)
-          .disabled(!MFMailComposeViewController.canSendMail())
-          .sheet(isPresented: $mailViewIsPresented) {
-            MailView(
-              messageBody: "This is a test iMessages string",
+          .disabled(!MFMessageComposeViewController.canSendText())
+          .sheet(isPresented: $iMessagesViewIsPresented) {
+            IMessagesView(
+              messageBody: "This is a test iMessage string",
               attachmentInfo: (
                 fileURL: TaskStore.shared.tasksDocURL,
-                mimeType: "application/xml"),
-              result: self.$result)
+                mimeType: "application/xml"
+              ),
+              result: self.$mailComposeResult
+            )
           }
 
           // Share Sheet
@@ -97,7 +104,8 @@ struct ContentView: View {
           .sheet(isPresented: $shareSheetIsPresented) {
             ShareSheet(
               activityItems: [TaskStore.shared.tasksDocURL],
-              excludedActivityTypes: [.copyToPasteboard])
+              excludedActivityTypes: [.copyToPasteboard]
+            )
           }
         }
       )
@@ -107,6 +115,6 @@ struct ContentView: View {
 
 struct ContentView_Previews: PreviewProvider {
   static var previews: some View {
-    ContentView( taskStore: TaskStore.shared )
+    ContentView(taskStore: TaskStore.shared)
   }
 }
--- TaskList/Views/IMessagesView.swift
+++ TaskList/Views/IMessagesView.swift
@@ -1,15 +1,15 @@
 /// Copyright (c) 2020 Razeware LLC
-/// 
+///
 /// Permission is hereby granted, free of charge, to any person obtaining a copy
 /// of this software and associated documentation files (the "Software"), to deal
 /// in the Software without restriction, including without limitation the rights
 /// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
 /// copies of the Software, and to permit persons to whom the Software is
 /// furnished to do so, subject to the following conditions:
-/// 
+///
 /// The above copyright notice and this permission notice shall be included in
 /// all copies or substantial portions of the Software.
-/// 
+///
 /// Notwithstanding the foregoing, you may not use, copy, modify, merge, publish,
 /// distribute, sublicense, create a derivative work, and/or sell copies of the
 /// Software in any work that is designed, intended, or marketed for pedagogical or
@@ -17,7 +17,7 @@
 /// or information technology.  Permission for such use, copying, modification,
 /// merger, publication, distribution, sublicensing, creation of derivative works,
 /// or sale is expressly withheld.
-/// 
+///
 /// This project and source code may use libraries or frameworks that are
 /// released under various Open-Source licenses. Use of those libraries and
 /// frameworks are governed by their own individual licenses.
@@ -30,16 +30,152 @@
 /// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 /// THE SOFTWARE.
 
+// import SwiftUI
+
+// struct IMessagesView: View {
+//    var body: some View {
+//        Text("Hello, iMessage World!")
+//    }
+// }
+//
+// struct IMessagesView_Previews: PreviewProvider {
+//    static var previews: some View {
+//        IMessagesView()
+//    }
+// }
+
+import MessageUI
 import SwiftUI
 
-struct IMessagesView: View {
-    var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
-    }
+// typealias AttachmentInfo = (fileURL: URL, mimeType: String)
+
+// public enum MFMailComposeResult : Int {
+//    case cancelled = 0
+//    case saved = 1
+//    case sent = 2
+//    case failed = 3
+// }
+
+// public enum MessageComposeResult : Int {
+//    case cancelled = 0
+//    case sent = 1
+//    case failed = 2
+// }
+
+enum MessageComposeError: Error {
+  case unknown
 }
 
-struct IMessagesView_Previews: PreviewProvider {
-    static var previews: some View {
-        IMessagesView()
+// TODO: replace MFMailComposeViewController by MFMessageComposeViewController
+
+struct IMessagesView: UIViewControllerRepresentable {
+  @Environment(\.presentationMode) var presentation
+  var messageBody: String
+  var attachmentInfo: AttachmentInfo?
+  @Binding var result: Result<MFMailComposeResult, Error>?
+
+  class Coordinator: NSObject, MFMessageComposeViewControllerDelegate {
+    @Binding var presentation: PresentationMode
+    @Binding var result: Result<MFMailComposeResult, Error>?
+    var messageBody: String
+    var attachmentInfo: AttachmentInfo?
+
+    init(
+      presentation: Binding<PresentationMode>,
+      messageBody: String,
+      attachmentInfo: AttachmentInfo?,
+      result: Binding<Result<MFMailComposeResult, Error>?>
+    ) {
+      _presentation = presentation
+      self.messageBody = messageBody
+      self.attachmentInfo = attachmentInfo
+      _result = result
     }
+
+//    func mailComposeController(
+//      _ controller: MFMailComposeViewController,
+//      didFinishWith result: MFMailComposeResult,
+//      error: Error?
+//    ) {
+//      defer {
+//        $presentation.wrappedValue.dismiss()
+//      }
+//      if let error = error {
+//        self.result = .failure(error)
+//        return
+//      }
+//      self.result = .success(result)
+//    }
+
+    func messageComposeViewController(_ controller: MFMessageComposeViewController, didFinishWith result: MessageComposeResult) {
+      defer {
+        $presentation.wrappedValue.dismiss()
+      }
+      if result == .sent {
+        self.result = .success(.sent)
+      } else {
+        self.result = .failure(MessageComposeError.unknown)
+      }
+    }
+  }
+
+  func makeCoordinator() -> Coordinator {
+    return Coordinator(
+      presentation: presentation,
+      messageBody: messageBody,
+      attachmentInfo: attachmentInfo,
+      result: $result
+    )
+  }
+
+  func makeUIViewController(
+    context: UIViewControllerRepresentableContext<IMessagesView>
+  ) -> MFMessageComposeViewController {
+    let viewController = MFMessageComposeViewController()
+    viewController.messageComposeDelegate = context.coordinator
+    // viewController.setMessageBody(context.coordinator.messageBody, isHTML: false)
+    // TODO: ^^^
+
+    if let fileURL = attachmentInfo?.fileURL,
+      let mimeType = attachmentInfo?.mimeType,
+      let fileData = try? Data(contentsOf: fileURL) {
+      viewController.addAttachmentData(
+        fileData,
+        typeIdentifier: mimeType,
+        filename: "ExportData.rwtl"
+      )
+    }
+    return viewController
+  }
+
+  func updateUIViewController(
+    _ uiViewController: MFMessageComposeViewController,
+    context: UIViewControllerRepresentableContext<IMessagesView>
+  ) {}
 }
+
+// func presentMessageVC(recipientPhoneNumbers: [String]) {
+//    guard MFMailComposeViewController.canSendMail() else {
+//        // TODO: Show alert informing the user
+//        printClassAndFunc(info: "canSendMail: false")
+//        return
+//    }
+//    // EP's Test Doesn't Work
+//    if !MFMessageComposeViewController.canSendText() {
+//        print("SMS services are not available")
+//    }
+//
+//    let messageVC = MFMessageComposeViewController()
+//    MFMessageComposeViewController.canSendAttachments() // EP's Test Doesn't Work
+//    messageVC.title = "\(String(describing: SharedUserDefaults.selectedCalendarTitle))'s Club" // EP's Test
+//    messageVC.body = ""
+//
+//    messageVC.recipients = recipientPhoneNumbers
+//
+//    messageVC.messageComposeDelegate = self
+//    messageVC.modalTransitionStyle = .crossDissolve
+//    messageVC.modalPresentationStyle = .fullScreen
+//
+//    present(messageVC, animated: true, completion: nil)
+// }
+//
